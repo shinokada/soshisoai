ab5f8186207662e30de0b22f4eff1abc4d794f22
shinokada <sokada@canacad.ac.jp>
2014-04-08 08:43:12 +0900

    Update

--- soshisoai.rb
+++ soshisoai.rb
@@ -1,51 +1,82 @@
 class Soshisoai
 
-  attr_reader :names, :choices, :arr, :combi, :firsttwo, :firstnew, :matches
+  attr_reader :arr, :matches, :combi
 
   def initialize 
     @arr=[]
-    @names=[]
-    @choices=[]
-    @firsttwo=[]
     @matches=[]
+    @combi = []
     file = File.new('soshisoai.txt', "r")
     file.each_line{|line| @arr<< line.strip }
-    @arr.each { |item| name, choices =item.split(':')
-                @names<<name
-                @choices<<choices.strip
-    }
+    # @arr.each { |item| name, choices =item.split(':')}
 
-    @combi = @arr.map do |e|
+    # @combi = @arr.map do |e|
+    #   before, afters = e.split(":")
+    #   afters.split(",").map{|after| "#{before}#{after}"}.join(",")
+    # end
+
+    # @firsttwo = @combi.map{ |item| item[0..1]}
+
+    # @firstnew = @firsttwo.map{|item| 
+    #   if item[0] =~ /[A-Z]/
+    #     item = item[1]+item[0]
+    #   else
+    #     item = item
+    #   end
+    # }
+    # @matches << @firstnew.detect{ |e| @firstnew.count(e) > 1}
+  end
+
+
+  def findmatches
+    input = getcombi(@arr) 
+    start = 0
+    getcolumn(input, start)
+    
+    # @arr.map do |e|
+      # before, afters = e.split(":")
+      # afters.split(",").map{|after| "#{before}#{after}"}.join(",")
+    # end
+    
+    # getcolumn(@combi, 0)
+
+    # .map{ |item| item[0..1]}
+    #   .map{|item| 
+    #   if item[0] =~ /[A-Z]/
+    #     item = item[1]+item[0]
+    #   else
+    #     item = item
+    #   end
+    # }
+    # @matches << firstnew.detect{ |e| firstnew.count(e) > 1}
+  end
+
+  def getcombi(arraylist)
+    arraylist.map do |e|
       before, afters = e.split(":")
       afters.split(",").map{|after| "#{before}#{after}"}.join(",")
     end
+  end
 
-    @firsttwo = @combi.map{ |item| item[0..1]}
 
-    @firstnew = @firsttwo.map{|item| 
+  def getcolumn(input,start)        
+    result = []
+    finishi = start + 1 
+    column = input.map{ |item| item[0..1]}
+      .map{|item| 
       if item[0] =~ /[A-Z]/
         item = item[1]+item[0]
       else
         item = item
       end
     }
-    @matches << @firstnew.detect{ |e| @firstnew.count(e) > 1}
-
-
+    result << column.detect{ |e| column.count(e) > 1}
   end
 
-
-  def combination
-    
-    @choices.split(",").map{ |letter| @name+letter}
-
-  end
-
-
-  def reversecombination
-    
-    @choices.split(",").map{ |letter| letter+@name}
-
+  def eliminate from item 
+    item1 =  item.join
+    item2 = item1.reverse
+    from.select { |pair| pair.include? item1 or pair.include? item2} 
 
   end
 
--- soshisoai_spec.rb
+++ soshisoai_spec.rb
@@ -1,21 +1,33 @@
 require './spec_helper'
 require './soshisoai.rb'
+arr = Soshisoai.new().arr
+arr2 = ["A:c,b,a", "B:a,b,d", "C:a,c,b", "D:d,a,c", "a:A,C,D", "b:D,A,B", "c:B,A,C", "d:D,C,A"]
+
 
 describe Soshisoai do
 
   specify{ expect(Soshisoai.new().arr).to eq ["A:c,b,a", "B:a,b,d", "C:a,c,b", "D:d,a,c", "a:A,C,D", "b:D,A,B", "c:B,A,C", "d:D,C,A"] }
+  
+  specify{ expect(arr).to eq ["A:c,b,a", "B:a,b,d", "C:a,c,b", "D:d,a,c", "a:A,C,D", "b:D,A,B", "c:B,A,C", "d:D,C,A"] }
 
-  specify{ expect(Soshisoai.new().names).to eq ["A", "B", "C", "D", "a", "b", "c", "d"] }
-  specify{ expect(Soshisoai.new().choices).to eq ["c,b,a", "a,b,d", "a,c,b", "d,a,c", "A,C,D", "D,A,B", "B,A,C", "D,C,A"] }
+  # specify{ expect(Soshisoai.new().names).to eq ["A", "B", "C", "D", "a", "b", "c", "d"] }
+  # specify{ expect(Soshisoai.new().choices).to eq ["c,b,a", "a,b,d", "a,c,b", "d,a,c", "A,C,D", "D,A,B", "B,A,C", "D,C,A"] }
   
-  specify{ expect(Soshisoai.new().combi).to eq ["Ac,Ab,Aa", "Ba,Bb,Bd", "Ca,Cc,Cb", "Dd,Da,Dc", "aA,aC,aD", "bD,bA,bB", "cB,cA,cC", "dD,dC,dA"] }
+  # specify{ expect(Soshisoai.new().combi).to eq ["Ac,Ab,Aa", "Ba,Bb,Bd", "Ca,Cc,Cb", "Dd,Da,Dc", "aA,aC,aD", "bD,bA,bB", "cB,cA,cC", "dD,dC,dA"] }
   
-  specify{ expect(Soshisoai.new().firsttwo).to eq ["Ac", "Ba", "Ca", "Dd", "aA", "bD", "cB", "dD"] }
+  # specify{ expect(Soshisoai.new().firsttwo).to eq ["Ac", "Ba", "Ca", "Dd", "aA", "bD", "cB", "dD"] }
   
-  specify{ expect(Soshisoai.new().firstnew).to eq ["cA", "aB", "aC", "dD", "aA", "bD", "cB", "dD"] }
+  # specify{ expect(Soshisoai.new().firstnew).to eq ["cA", "aB", "aC", "dD", "aA", "bD", "cB", "dD"] }
   
-  specify{ expect(Soshisoai.new().matches).to eq ["dD"] }
+  # specify{ expect(Soshisoai.new().matches).to eq ["dD"] }
 
-
+  specify{ expect(Soshisoai.new().getcombi(arr)).to eq ["Ac,Ab,Aa", "Ba,Bb,Bd", "Ca,Cc,Cb", "Dd,Da,Dc", "aA,aC,aD", "bD,bA,bB", "cB,cA,cC", "dD,dC,dA"] }
+  
+  # specify{ expect(Soshisoai.new().findmatches).to eq ["cA", "aB", "aC", "dD", "aA", "bD", "cB", "dD"] }
+  
+  specify{ expect(Soshisoai.new().findmatches).to eq ["dD"] }
+  
+  specify{ expect(Soshisoai.new().result).to eq ["dD"] }
+ 
 
 end
