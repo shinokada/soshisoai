7b4bfd42fdcf8e8f85ebe5952671af961f33c557
Junichi Ito <jit@sonicgarden.jp>
2014-04-10 12:24:32 +0900

    Refactor spec.

--- soshisoai1_spec.rb
+++ soshisoai1_spec.rb
@@ -1,29 +1,30 @@
 require './spec_helper'
 require './soshisoai1.rb'
 
-myarr = Soshisoai.new ('./soshisoai.txt')
-myarr1 = myarr.initial_arr
-myarr2 = myarr.get_combi(myarr1)
-arr2 = ["Ac", "Ac", "Aa", "Ba", "Bb"]
-myarr3 = myarr.flat_arr(myarr2)
-myarr4 = myarr.swap_male(myarr3)
-myarr5 = myarr.find_match(myarr4)
-
 describe Soshisoai do
+  before do
+    @my_arr = Soshisoai.new ('./soshisoai.txt')
+    @my_arr1 = @my_arr.initial_arr
+    @my_arr2 = @my_arr.get_combi(@my_arr1)
+    @arr2 = ["Ac", "Ac", "Aa", "Ba", "Bb"]
+    @my_arr3 = @my_arr.flat_arr(@my_arr2)
+    @my_arr4 = @my_arr.swap_male(@my_arr3)
+    @my_arr5 = @my_arr.find_match(@my_arr4)
+  end
 
-  specify{ expect(myarr.initial_arr).to eq ["A:c,b,a", "B:a,b,d", "C:a,c,b", "D:d,a,c", "a:A,C,D", "b:D,A,B", "c:B,A,C", "d:D,C,A"] }
-  
-  specify{ expect(myarr.get_combi(myarr1)).to eq ["Ac,Ab,Aa", "Ba,Bb,Bd", "Ca,Cc,Cb", "Dd,Da,Dc", "aA,aC,aD", "bD,bA,bB", "cB,cA,cC", "dD,dC,dA"] }
-  
-  specify{ expect(myarr.flat_arr(myarr2)).to eq ["Ac", "Ab", "Aa", "Ba", "Bb", "Bd", "Ca", "Cc", "Cb", "Dd", "Da", "Dc", "aA", "aC", "aD", "bD", "bA", "bB", "cB", "cA", "cC", "dD", "dC", "dA"]}
+  specify{ expect(@my_arr.initial_arr).to eq ["A:c,b,a", "B:a,b,d", "C:a,c,b", "D:d,a,c", "a:A,C,D", "b:D,A,B", "c:B,A,C", "d:D,C,A"] }
 
-  specify{ expect(myarr.eliminate_duplicate(arr2)).to eq ["Aa", "Ba", "Bb"]}
+  specify{ expect(@my_arr.get_combi(@my_arr1)).to eq ["Ac,Ab,Aa", "Ba,Bb,Bd", "Ca,Cc,Cb", "Dd,Da,Dc", "aA,aC,aD", "bD,bA,bB", "cB,cA,cC", "dD,dC,dA"] }
+
+  specify{ expect(@my_arr.flat_arr(@my_arr2)).to eq ["Ac", "Ab", "Aa", "Ba", "Bb", "Bd", "Ca", "Cc", "Cb", "Dd", "Da", "Dc", "aA", "aC", "aD", "bD", "bA", "bB", "cB", "cA", "cC", "dD", "dC", "dA"]}
+
+  specify{ expect(@my_arr.eliminate_duplicate(@arr2)).to eq ["Aa", "Ba", "Bb"]}
   
-  specify{ expect(myarr.swap_male(myarr3)).to eq ["cA", "bA", "aA", "aB", "bB","dB", "aC", "cC", "bC", "dD", "aD", "cD", "aA", "aC", "aD", "bD", "bA", "bB", "cB", "cA", "cC", "dD", "dC", "dA"]}
+  specify{ expect(@my_arr.swap_male(@my_arr3)).to eq ["cA", "bA", "aA", "aB", "bB","dB", "aC", "cC", "bC", "dD", "aD", "cD", "aA", "aC", "aD", "bD", "bA", "bB", "cB", "cA", "cC", "dD", "dC", "dA"]}
   
-  specify{ expect(myarr.find_match(myarr4)).to eq ["cA", "bA", "aA", "bB", "aC", "cC", "dD", "aD"]}
+  specify{ expect(@my_arr.find_match(@my_arr4)).to eq ["cA", "bA", "aA", "bB", "aC", "cC", "dD", "aD"]}
   
-  specify{ expect(myarr.delete_same_suffix(myarr5)).to eq ["cA", "bB", "aC", "dD"]}
+  specify{ expect(@my_arr.delete_same_suffix(@my_arr5)).to eq ["cA", "bB", "aC", "dD"]}
 
 
 end
