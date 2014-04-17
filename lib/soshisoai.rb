56279a62b5e7564273484638f6f2590f41e084db
shinokada <okada.shin@gmail.com>
2014-04-17 12:41:08 +0900

    Cleaning code

--- lib/soshisoai.rb
+++ lib/soshisoai.rb
@@ -1,19 +1,27 @@
+##
+# Find best matching when three preferences are given in a file
 class Soshisoai
-
+  ##
+  # Parse a file and create an arry
+  # @param file_name [File]
+  # @return [Array]
   def self.parse_file(file_name)
-    file = File.new(file_name, "r")
+    file = File.new(file_name, 'r')
     @initial_arr = file.each_line.map(&:strip)
   end
-
+  ##
+  # Create an array of combination such as Ac, Aa, Ab etc
+  # @returen [Array]
   def self.get_combi(array_list)
     array_list.map do |e|
-      before, afters = e.split(":")
-      if afters
-        [before].product(afters.split(",")).map(&:join).join(',')
-      end
+      before, afters = e.split(':')
+      [before].product(afters.split(',')).map(&:join).join(',') if afters
     end
   end
-
+  ##
+  # Flaten an array
+  # @param arr [Array]
+  # @returen [Array]
   def self.flat_arr(arr)
     arr.compact.map { |s| s.split(',') }.flatten
   end
--- spec/lib/soshisoai_spec.rb
+++ spec/lib/soshisoai_spec.rb
@@ -10,23 +10,23 @@ describe Soshisoai do
       @my_arr = Soshisoai.parse_file(target_file)
       @my_arr1 = Soshisoai.parse_file(target_file)
       @my_arr2 = Soshisoai.get_combi(@my_arr1)
-      @arr2 = ["Ac", "Ac", "Aa", "Ba", "Bb"]
+      @arr2 = ['Ac', 'Ac', 'Aa', 'Ba', 'Bb']
       @my_arr3 = Soshisoai.flat_arr(@my_arr2)
       @my_arr4 = Soshisoai.swap_male(@my_arr3)
       @my_arr5 = Soshisoai.find_match(@my_arr4)
     end
 
-    specify{ expect(Soshisoai.get_combi(@my_arr1)).to eq ["Ac,Ab,Aa", "Ba,Bb,Bd", "Ca,Cc,Cb", "Dd,Da,Dc", "aA,aC,aD", "bD,bA,bB", "cB,cA,cC", "dD,dC,dA"] }
+    specify { expect(Soshisoai.get_combi(@my_arr1)).to eq ['Ac,Ab,Aa', 'Ba,Bb,Bd', 'Ca,Cc,Cb', 'Dd,Da,Dc', 'aA,aC,aD', 'bD,bA,bB', 'cB,cA,cC', 'dD,dC,dA']  }
 
-    specify{ expect(Soshisoai.flat_arr(@my_arr2)).to eq ["Ac", "Ab", "Aa", "Ba", "Bb", "Bd", "Ca", "Cc", "Cb", "Dd", "Da", "Dc", "aA", "aC", "aD", "bD", "bA", "bB", "cB", "cA", "cC", "dD", "dC", "dA"]}
+    specify { expect(Soshisoai.flat_arr(@my_arr2)).to eq ['Ac', 'Ab', 'Aa', 'Ba', 'Bb', 'Bd', 'Ca', 'Cc', 'Cb', 'Dd', 'Da', 'Dc', 'aA', 'aC', 'aD', 'bD', 'bA', 'bB', 'cB', 'cA', 'cC', 'dD', 'dC', 'dA'] }
 
-    specify{ expect(Soshisoai.eliminate_duplicate(@arr2)).to eq ["Aa", "Ba", "Bb"]}
+    specify { expect(Soshisoai.eliminate_duplicate(@arr2)).to eq ['Aa', 'Ba', 'Bb'] }
 
-    specify{ expect(Soshisoai.swap_male(@my_arr3)).to eq ["cA", "bA", "aA", "aB", "bB","dB", "aC", "cC", "bC", "dD", "aD", "cD", "aA", "aC", "aD", "bD", "bA", "bB", "cB", "cA", "cC", "dD", "dC", "dA"]}
+    specify { expect(Soshisoai.swap_male(@my_arr3)).to eq ['cA', 'bA', 'aA', 'aB', 'bB', 'dB', 'aC', 'cC', 'bC', 'dD', 'aD', 'cD', 'aA', 'aC', 'aD', 'bD', 'bA', 'bB', 'cB', 'cA', 'cC', 'dD', 'dC', 'dA'] }
 
-    specify{ expect(Soshisoai.find_match(@my_arr4)).to eq ["cA", "bA", "aA", "bB", "aC", "cC", "dD", "aD"]}
+    specify { expect(Soshisoai.find_match(@my_arr4)).to eq ['cA', 'bA', 'aA', 'bB', 'aC', 'cC', 'dD', 'aD'] }
 
-    specify{ expect(Soshisoai.delete_same_suffix(@my_arr5)).to eq ["cA", "bB", "aC", "dD"]}
+    specify { expect(Soshisoai.delete_same_suffix(@my_arr5)).to eq ['cA', 'bB', 'aC', 'dD'] }
   end
 
   context 'soshisoai2.txt' do
@@ -38,7 +38,7 @@ describe Soshisoai do
       @my_arr = Soshisoai.parse_file(target_file)
       @my_arr1 = Soshisoai.parse_file(target_file)
       @my_arr2 = Soshisoai.get_combi(@my_arr1)
-      @arr2 = ["Ac", "Ac", "Aa", "Ba", "Bb"]
+      @arr2 = ['Ac', 'Ac', 'Aa', 'Ba', 'Bb']
       @my_arr3 = Soshisoai.flat_arr(@my_arr2)
       @my_arr4 = Soshisoai.swap_male(@my_arr3)
       @my_arr5 = Soshisoai.find_match(@my_arr4)
@@ -46,17 +46,17 @@ describe Soshisoai do
     end
 
 
-    specify{ expect(Soshisoai.get_combi(@my_arr1)).to eq ["Ac,Aa,Ab", "Bc,Bf,Ba", "Cf,Cc,Cb", "Dd,Dd,Dd", nil, "Fe,Fc,Fa", "aA,aD,aF", "bC,bB,bA", "cD,cA,cC", "dA,dA,dB", "eC,eA,eE", "fD,fB,fA"]}
+    specify { expect(Soshisoai.get_combi(@my_arr1)).to eq ['Ac,Aa,Ab', 'Bc,Bf,Ba', 'Cf,Cc,Cb', 'Dd,Dd,Dd', nil, 'Fe,Fc,Fa', 'aA,aD,aF', 'bC,bB,bA', 'cD,cA,cC', 'dA,dA,dB', 'eC,eA,eE', 'fD,fB,fA'] }
 
-    specify{ expect(Soshisoai.flat_arr(@my_arr2)).to eq ["Ac", "Aa", "Ab", "Bc", "Bf", "Ba", "Cf", "Cc", "Cb", "Dd", "Dd", "Dd", "Fe", "Fc", "Fa", "aA", "aD", "aF", "bC", "bB", "bA", "cD", "cA", "cC", "dA", "dA", "dB", "eC", "eA", "eE", "fD", "fB", "fA"]}
+    specify { expect(Soshisoai.flat_arr(@my_arr2)).to eq ['Ac', 'Aa', 'Ab', 'Bc', 'Bf', 'Ba', 'Cf', 'Cc', 'Cb', 'Dd', 'Dd', 'Dd', 'Fe', 'Fc', 'Fa', 'aA', 'aD', 'aF', 'bC', 'bB', 'bA', 'cD', 'cA', 'cC', 'dA', 'dA', 'dB', 'eC', 'eA', 'eE', 'fD', 'fB', 'fA'] }
 
-    specify{ expect(Soshisoai.swap_male(@my_arr3)).to eq ["cA", "aA", "bA", "cB", "fB", "aB", "fC", "cC", "bC", "dD", "dD", "dD", "eF", "cF", "aF", "aA", "aD", "aF", "bC", "bB", "bA", "cD", "cA", "cC", "dA", "dA", "dB", "eC", "eA", "eE", "fD", "fB", "fA"]}
+    specify { expect(Soshisoai.swap_male(@my_arr3)).to eq ['cA', 'aA', 'bA', 'cB', 'fB', 'aB', 'fC', 'cC', 'bC', 'dD', 'dD', 'dD', 'eF', 'cF', 'aF', 'aA', 'aD', 'aF', 'bC', 'bB', 'bA', 'cD', 'cA', 'cC', 'dA', 'dA', 'dB', 'eC', 'eA', 'eE', 'fD', 'fB', 'fA'] }
   
-    specify{ expect(Soshisoai.find_match(@my_arr4)).to eq ["cA", "aA", "bA", "fB", "cC", "bC", "dD", "aF", "dA"]}
+    specify { expect(Soshisoai.find_match(@my_arr4)).to eq ['cA', 'aA', 'bA', 'fB', 'cC', 'bC', 'dD', 'aF', 'dA'] }
   
-    specify{ expect(Soshisoai.delete_same_suffix(@my_arr5)).to eq ["cA", "fB", "cC", "dD", "aF"]}
+    specify { expect(Soshisoai.delete_same_suffix(@my_arr5)).to eq ['cA', 'fB', 'cC', 'dD', 'aF'] }
  
-    specify{ expect(Soshisoai.delete_same_prefix(@my_arr6)).to eq ["cA", "fB", "dD", "aF"]}
+    specify { expect(Soshisoai.delete_same_prefix(@my_arr6)).to eq ['cA', 'fB', 'dD', 'aF'] }
   end
 
 end
