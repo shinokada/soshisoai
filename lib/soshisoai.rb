b8138ca7b827f51f198d200be73e5b6b673edd9b
shinokada <okada.shin@gmail.com>
2014-04-17 21:09:34 +0900

    Added documentations

--- lib/soshisoai.rb
+++ lib/soshisoai.rb
@@ -19,29 +19,48 @@ class Soshisoai
     end
   end
   ##
-  # Flaten an array
+  # Flaten an array after removing nil elements by Array#compact
   # @param arr [Array]
-  # @returen [Array]
+  # @returen [Array] one array like ['Ac', 'Ab']
   def self.flat_arr(arr)
     arr.compact.map { |s| s.split(',') }.flatten
   end
 
+  ##
+  # This will eliminate duplicates from an array
+  # @param arr [Array]
+  # @return [Array]
   def self.eliminate_duplicate(arr)
     arr.reject { |e| arr.count(e) > 1 }
   end
 
+  ##
+  # Change male input Ac to aC and return all array included female
+  # @param arr {Array}
+  # @return [Array] like ['cA', 'bA']
   def self.swap_male(arr)
     arr.map { |item| item =~ /^[A-Z]/ ? item.reverse : item }
   end
 
+  ##
+  # Find all of item which counts more than 1 and remove duplicates by Array#uniq
+  # @param arr [Array]
+  # @return [Array]
   def self.find_match(arr)
     arr.find_all { |e| arr.count(e) > 1 }.uniq
   end
 
+  ##
+  # This deletes the same suffix i.e. bC aC and delete aC
+  # @param arr [Array]
+  # @return [Array]
   def self.delete_same_suffix(arr)
     arr.group_by { |s| s[-1] }.values.map(&:first)
   end
 
+  ## 
+  # This deletes the same prefix i.e. aC aD delete aD
+  # @return [Array]
   def self.delete_same_prefix(arr)
     arr.group_by { |s| s[0] }.values.map(&:first)
   end
