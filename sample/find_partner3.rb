a5dc7906916f6422c4a111c51e1c3eb5df56a554
shinokada <okada.shin@gmail.com>
2014-04-17 20:38:11 +0900

    Update sample 3

--- sample/find_partner3.rb
+++ sample/find_partner3.rb
@@ -1,16 +1,22 @@
 require_relative '../lib/soshisoai'
 require 'logger'
 
-$Log = Logger.new('log_file.log')
-$Log.debug('Soshisoai3')
-file_path = File.expand_path('../../spec/fixtures/soshisoai3.txt', __FILE__)
+class MyApp
+  $logger = Logger.new('log_file.log')
+  def self.logger
+  $logger.debug('Soshisoai3')
+  file_path = File.expand_path('../../spec/fixtures/soshisoai3.txt', __FILE__)
 
-myarr1 = Soshisoai.parse_file(file_path)
-myarr2 = Soshisoai.get_combi(myarr1)
-myarr3 = Soshisoai.flat_arr(myarr2)
-myarr4 = Soshisoai.eliminate_duplicate(myarr3)
-myarr5 = Soshisoai.swap_male(myarr4)
-myarr6 = Soshisoai.find_match(myarr5)
-myarr7 = Soshisoai.delete_same_suffix(myarr6)
-myarr8 = Soshisoai.delete_same_prefix(myarr7)
-$Log.debug(myarr8)
+  myarr1 = Soshisoai.parse_file(file_path)
+  myarr2 = Soshisoai.get_combi(myarr1)
+  myarr3 = Soshisoai.flat_arr(myarr2)
+  myarr4 = Soshisoai.eliminate_duplicate(myarr3)
+  myarr5 = Soshisoai.swap_male(myarr4)
+  myarr6 = Soshisoai.find_match(myarr5)
+  myarr7 = Soshisoai.delete_same_suffix(myarr6)
+  myarr8 = Soshisoai.delete_same_prefix(myarr7)
+  $logger.debug(myarr8)
+  end
+end
+
+MyApp.logger
